679251040318e1b0597b74570074f4398c5ac94c
YuriManhica <156890528+YuriManhica@users.noreply.github.com>
2025-02-21 10:52:18 +0200

    feat:ensure same product can be added more that once to cart

--- src/app/components/ui/scroll-area.tsx
+++ src/app/components/ui/scroll-area.tsx
@@ -35,12 +35,18 @@ const ScrollBar = React.forwardRef<
       orientation === "vertical" &&
         "h-full w-2.5 border-l border-l-transparent p-[1px]",
       orientation === "horizontal" &&
-        "h-2.5 flex-col border-t border-t-transparent p-[1px]",
+        "h-2.5 border-t border-t-transparent p-[1px]",
       className
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb
+      className={cn(
+        "relative flex-1 rounded-full bg-border",
+        orientation === "horizontal" && "min-h-[0.5rem]",
+        orientation === "vertical" && "min-w-[0.5rem]"
+      )}
+    />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
